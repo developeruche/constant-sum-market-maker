4f731116684b5946e45e423b03607b2825baca9d
Developer Uche <nweleuchenna2004@gmail.com>
2022-12-23 08:23:23 +0100

    developeruche: done interfacing

---
+++ contracts/ConstantSumMarketMaker.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+/// @notice this is a solidity project that implements a market maket whose formula of opeartion is x+y=k
+contract ConstantSumMarketMaker {
+    function swap() external {
+
+    }
+
+    function addLiquidity() external {
+
+    }
+
+    function removeLiquidity() external {
+        
+    }
+}
